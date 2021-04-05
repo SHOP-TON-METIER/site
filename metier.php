acef7c0fa8f17ff038537d012fcecbd4af6dda29
AurelienLouvel <louvel.aurelien.perso@gmail.com>
2021-04-05 16:02:31 +0200

    Update metier.php

--- metier.php
+++ metier.php
@@ -6,7 +6,23 @@ if (isset($_POST['id'])) {
 
     $id = $_POST['id'];
 
-    $sql = "SELECT * from metier WHERE id_metier = $id";
+    $sql = "SELECT * from metier WHERE id_metier = $id;
+    
+    
+    SELECT    m.nom, m.description, m.salaire, m.code, etud.nom, etud.prenom, etud.promotionMMI, etud.avis, etud.conseil, etud.adjectifs,c.nom, c.type, f.nom, f.anneesEtudes
+    FROM     metier AS m ,
+        ancienEtudiant AS etud,
+        competence AS c,
+        rel_metier_comp AS rmc,
+    formation AS f,
+    rel_form_metier AS rfm
+    
+    WHERE  etud.id_metier = m.identifiant
+    AND      m.identifiant = rmc.id_metier
+    AND        c.identifiant = rmc.id_competence
+     AND       f.identifiant_formation = rfm.id_formation
+    AND        m.identifiant = rfm.id_metier
+    AND m.identifiant LIKE $id";
     $req = $link->prepare($sql);
 
     $req->execute();
