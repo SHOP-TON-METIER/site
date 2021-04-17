6fa7206659609b3eaf4af24f9ec90109035dc2c4
AurelienLouvel <louvel.aurelien.perso@gmail.com>
2021-04-17 03:24:02 +0200

    Ajustements Page Métier

--- metier.php
+++ metier.php
@@ -42,7 +42,7 @@
             <h2>Salaire</h2>
 
             <p class="salaire" id="salaire"></p>
-        
+
             <h2>Compétences</h2>
 
             <ul id="competences"></ul>
@@ -57,207 +57,207 @@
                 <span>Michel</span><br>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt mollitia earum neque ullam reiciendis labore
                 exercitationem fugit assumenda ipsa! Explicabo libero quibusdam ad facere enim impedit, facilis pariatur sint
-                </p>
+            </p>
 
-                <p class="avis">
+            <p class="avis">
                 <span>Michel</span><br>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt mollitia earum neque ullam reiciendis labore
                 exercitationem fugit assumenda ipsa! Explicabo libero quibusdam ad facere enim impedit, facilis pariatur sint
-                </p>
+            </p>
 
-                <p class="avis">
+            <p class="avis">
                 <span>Michel</span><br>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt mollitia earum neque ullam reiciendis labore
                 exercitationem fugit assumenda ipsa! Explicabo libero quibusdam ad facere enim impedit, facilis pariatur sint
-                </p>
+            </p>
 
         </section>
     </main>
 
     <script src="js/three.min.js"></script>
-        <script src="js/GLTFLoader.js"></script>
-        <script src="js/OrbitControls.js"></script>
-        <script>
-            var camera, scene, renderer, avatar
+    <script src="js/GLTFLoader.js"></script>
+    <script src="js/OrbitControls.js"></script>
+    <script>
+        var camera, scene, renderer, avatar
 
-            //CREATE SCENE
+        //CREATE SCENE
 
-            function createScene() {
-                WIDTH = 200
-                HEIGHT = document.querySelector(".avatar").clientHeight
+        function createScene() {
+            WIDTH = 200
+            HEIGHT = document.querySelector(".avatar").clientHeight
 
-                scene = new THREE.Scene()
-                camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000)
-                camera.position.z = 5.5
-                scene.position.y = -2.5
+            scene = new THREE.Scene()
+            camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000)
+            camera.position.z = 5.5
+            scene.position.y = -2.5
 
 
-                const ambientLight = new THREE.AmbientLight(0xffffff, 2)
-                scene.add(ambientLight)
+            const ambientLight = new THREE.AmbientLight(0xffffff, 2)
+            scene.add(ambientLight)
 
-                avatar = document.querySelector(".avatar")
+            avatar = document.querySelector(".avatar")
 
-                renderer = new THREE.WebGLRenderer({
-                    antialias: true
-                })
+            renderer = new THREE.WebGLRenderer({
+                antialias: true
+            })
 
-                renderer.setSize(WIDTH, HEIGHT)
-                renderer.setClearColor(0xffffff)
-                avatar.appendChild(renderer.domElement)
+            renderer.setSize(WIDTH, HEIGHT)
+            renderer.setClearColor(0xffffff)
+            avatar.appendChild(renderer.domElement)
 
-                window.addEventListener('resize', handleWindowResize, false)
+            window.addEventListener('resize', handleWindowResize, false)
 
-            }
+        }
 
-            //RESIZE
+        //RESIZE
 
-            function handleWindowResize() {
-                HEIGHT = document.querySelector(".avatar").clientHeight
-                renderer.setSize(WIDTH, HEIGHT)
-                camera.aspect = WIDTH / HEIGHT
-                camera.updateProjectionMatrix()
-            }
+        function handleWindowResize() {
+            HEIGHT = document.querySelector(".avatar").clientHeight
+            renderer.setSize(WIDTH, HEIGHT)
+            camera.aspect = WIDTH / HEIGHT
+            camera.updateProjectionMatrix()
+        }
 
-            //CREATE OBJECT
+        //CREATE OBJECT
 
-            function createObject() {
-                const loader = new THREE.GLTFLoader();
-                const url = 'gltf/characters/perso/scene.gltf'
+        function createObject() {
+            const loader = new THREE.GLTFLoader();
+            const url = 'gltf/characters/perso/scene.gltf'
 
-                loader.load(
-                    url,
+            loader.load(
+                url,
 
-                    function(gltf) {
-                        model = gltf.scene
-                        scene.add(model)
-                    }
-                )
-            }
+                function(gltf) {
+                    model = gltf.scene
+                    scene.add(model)
+                }
+            )
+        }
 
-            //CONTROLS
+        //CONTROLS
 
-            function createControls() {
-                controls = new THREE.OrbitControls(camera, renderer.domElement)
-                controls.enableZoom = false
-                controls.enablePan = false
-                controls.enableDamping = true
-                controls.minPolarAngle = Math.PI / 2
-                controls.maxPolarAngle = Math.PI / 2
-                controls.autoRotate = true
-                controls.autoRotateSpeed = 10
-            }
+        function createControls() {
+            controls = new THREE.OrbitControls(camera, renderer.domElement)
+            controls.enableZoom = false
+            controls.enablePan = false
+            controls.enableDamping = true
+            controls.minPolarAngle = Math.PI / 2
+            controls.maxPolarAngle = Math.PI / 2
+            controls.autoRotate = true
+            controls.autoRotateSpeed = 10
+        }
 
-            function loop() {
-                controls.update()
-                renderer.render(scene, camera)
-                requestAnimationFrame(loop)
-            }
+        function loop() {
+            controls.update()
+            renderer.render(scene, camera)
+            requestAnimationFrame(loop)
+        }
 
-            function init() {
-                createScene()
-                createControls()
-                createObject()
-                loop()
-            }
+        function init() {
+            createScene()
+            createControls()
+            createObject()
+            loop()
+        }
 
-            window.addEventListener('load', init, false)
-        </script>
+        window.addEventListener('load', init, false)
+    </script>
 
     <script>
-            var id = 2
+        var id = 2
 
-            function get_metier(data) {
-                console.log(data)
-                var request = $.ajax({
-                    url: "api_metier.php",
-                    method: "GET",
-                    data: {
-                        id: id,
-                        type: "competences"
-                    },
-                    dataType: "JSON"
+        function get_metier(data) {
+            console.log(data)
+            var request = $.ajax({
+                url: "api_metier.php",
+                method: "GET",
+                data: {
+                    id: id,
+                    type: "competences"
+                },
+                dataType: "JSON"
+            })
+
+            request.done(function(data) {
+                $.each(data, function(index, item) {
+                    $('#competences').append("<li>" + item.competences + "</li>")
                 })
+            }, get_competences)
 
-                request.done(function(data) {
-                    $.each(data, function(index, item) {
-                        $('#competences').append("<li>" + item.competences + "</li>")
-                    })
-                }, get_competences)
+            request.fail(function(jqXHR, textStatus) {
+                alert("Request failed: " + textStatus)
+            })
+        }
 
-                request.fail(function(jqXHR, textStatus) {
-                    alert("Request failed: " + textStatus)
+        function get_competences(data) {
+            console.log(data)
+            var request = $.ajax({
+                url: "api_metier.php",
+                method: "GET",
+                data: {
+                    id: id,
+                    type: "formations"
+                },
+                dataType: "JSON"
+            })
+
+            request.done(function(data) {
+                $.each(data, function(index, item) {
+                    $('#formations').append("<li>" + item.formation + "</li>")
                 })
-            }
+            }, get_formations)
 
-            function get_competences(data) {
-                console.log(data)
-                var request = $.ajax({
-                    url: "api_metier.php",
-                    method: "GET",
-                    data: {
-                        id: id,
-                        type: "formations"
-                    },
-                    dataType: "JSON"
-                })
+            request.fail(function(jqXHR, textStatus) {
+                alert("Request failed: " + textStatus)
+            })
+        }
 
-                request.done(function(data) {
-                    $.each(data, function(index, item) {
-                        $('#formations').append("<li>" + item.formation + "</li>")
-                    })
-                }, get_formations)
+        function get_formations(data) {
+            console.log(data)
+            var request = $.ajax({
+                url: "api_metier.php",
+                method: "GET",
+                data: {
+                    id: id,
+                    type: "etudiants"
+                },
+                dataType: "JSON"
+            })
 
-                request.fail(function(jqXHR, textStatus) {
-                    alert("Request failed: " + textStatus)
-                })
-            }
+            request.done(get_etudiants)
 
-            function get_formations(data) {
-                console.log(data)
-                var request = $.ajax({
-                    url: "api_metier.php",
-                    method: "GET",
-                    data: {
-                        id: id,
-                        type: "etudiants"
-                    },
-                    dataType: "JSON"
-                })
+            request.fail(function(jqXHR, textStatus) {
+                alert("Request failed: " + textStatus)
+            })
+        }
 
-                request.done(get_etudiants)
+        function get_etudiants(data) {
+            console.log(data)
+        }
+        $(document).ready(function($) {
 
-                request.fail(function(jqXHR, textStatus) {
-                    alert("Request failed: " + textStatus)
-                })
-            }
+            var request = $.ajax({
+                url: "api_metier.php",
+                method: "GET",
+                data: {
+                    id: id,
+                    type: "metier"
+                },
+                dataType: "JSON"
+            })
 
-            function get_etudiants(data) {
-                console.log(data)
-            }
-            $(document).ready(function($) {
+            request.done(function(data) {
+                $('#nom').html(data[0].nom)
+                $('#metier').html('Le metier de ' + data[0].nom.toLowerCase())
+                $('#description').html(data[0].description)
+                $('#salaire').html(data[0].salaire)
+            }, get_metier)
 
-                var request = $.ajax({
-                    url: "api_metier.php",
-                    method: "GET",
-                    data: {
-                        id: id,
-                        type: "metier"
-                    },
-                    dataType: "JSON"
-                })
-
-                request.done(function(data) {
-                    $('#nom').html(data[0].nom)
-                    $('#metier').html('Le metier de ' + data[0].nom.toLowerCase())
-                    $('#description').html(data[0].description)
-                    $('#salaire').html(data[0].salaire)
-                }, get_metier)
-
-                request.fail(function(jqXHR, textStatus) {
-                    alert("Request failed: " + textStatus)
-                })
-            });
-        </script>
+            request.fail(function(jqXHR, textStatus) {
+                alert("Request failed: " + textStatus)
+            })
+        });
+    </script>
 </body>
 
 </html>
--- styles_header.php
+++ styles_header.php
@@ -1,88 +1,72 @@
-<?php 
-    echo ('<style>
-        *,
-        *::before,
-        *::after {
-            box-sizing: border-box;
-        }
-    
-        body {
-            margin: 0;
-            font-family: \'Poppins\', sans-serif;
-        } 
+<?php
+echo ('<style>
+            
+    header {
+        top: 0;
+        height: 80px;
+        width: 94vw;
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #ffffff;
+        z-index: 1;
+    }
 
-        header {
-            height: 80px;
-            width: 100vw;
-            border-bottom: 1px solid #848C96;
-            position: fixed;
-            top: 0;
-            background-color: #FFF;
-            z-index: 1;
-        }
+    .logo {
+        width: 68px;
+        height: 40px;
+    }
+
+    .right {
+        width: 300px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         
-        .logo {
-            margin-top: 0;
-            width: 68px;
-            position: absolute;
-            top: 20px;
-            left: 40px;
-        }
-        
-        header div {
-            position: absolute;
-        }
-        
-        .bag {
-            top: 26px;
-            right: 40px;
-        }
-        
-        .bag svg{
-            width: 36px;
-            height: 36px;
-            color: #09192C;
-        }
-        
-        .search {
-            top: 25px;
-            right: 155px;
-        }
-        
-        .search input {
-            width: 160px;
-            height: 40px;
-            margin: auto;
-            border: solid 2px #000;
-            border-radius: 40px;
-            padding-left: 35px;
-            font-size: 0.875rem;
-        }
-        
-        .search input:focus {
-            outline: none;
-            box-shadow: #848C96 2px 2px 2px;
-        }
-        
-        .search svg{
-            width: 20px;
-            height: 20px;
-            color: #848C96;
-            position: absolute;
-            top: 10px;
-            left: 7px;
-        }
-        
-        .exit {
-            position: fixed;
-            top: 120px;
-            left: 20px;
-        }
-        
-        .exit svg {
-            width: 70px;
-            height: 70px;
-            color: #09192C;
-        }
-    </style>')
-?>
+    }
+
+    .bag { 
+        width: 36px;
+        height: 36px;
+        color: #09192C;
+    }
+
+    .searchbar{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        cursor: text;
+        margin: 0 auto;
+        padding: 6px 10px;
+        border-radius: 24px;
+        border: solid 2px #09192C;
+    }
+
+    .searchbar input {
+        box-sizing: border-box;
+        margin-left: 10px;
+        width: 120px;
+        border-color: transparent;
+        background-color: transparent;
+        color:  #848C96;
+        font-size: 0.875rem;
+        user-select: none;
+        font-family: "Poppins";
+    }
+            
+    .searchbar input:focus {
+        outline: none;
+        color:  #09192C;
+    }
+
+    .icon{
+        margin: auto;
+        width: 24px;
+        color: #848C96;
+    }
+
+    .icon svg{
+        vertical-align: middle;
+    }
+    </style>');
